4624a7f022f833162ed7d5ccb3a81b54aa58d8bd
Robin Linacre <robinlinacre@hotmail.com>
2026-01-23 11:17:40 +0000

    progress

--- test-notebooks/my-custom-lib/index.js
+++ test-notebooks/my-custom-lib/index.js
@@ -2,3 +2,15 @@
 export default function hello_world() {
     return "Hello from my-custom-lib!";
 }
+
+function observe_chart_data(chart, signal_name) {
+    return Generators.observe(function (notify) {
+        const signaled = (name, value) => notify(value);
+        chart.addSignalListener(signal_name, signaled);
+
+        notify(chart.signal(signal_name));
+
+        return () => chart.removeSignalListener(signal_name, signaled);
+    });
+}
+export { observe_chart_data };
